40c71dfc9f11682ceb30f059aabb3c80e33fab16
Elliot Chung <ec2018@gmail.com>
2022-02-20 23:15:21 -0800

    Setup linting for backend

--- backend/index.ts
+++ backend/index.ts
@@ -6,18 +6,15 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-app.use(express.json({ extended: true }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(`${process.env.CONNECTION_URL}`)
   .then(() =>
     app.listen(PORT, () => console.log(`It's alive on port: ${PORT}`))
   )
-  .catch((err) => console.log(err));
+  .catch(err => console.log(err));
