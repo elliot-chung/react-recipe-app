73ba7a86c9e959b563d62f99ecbd720c6e360d5b
Elliot Chung <ec2018@gmail.com>
2022-02-20 20:50:45 -0800

    Removed body-parser

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,22 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.use(bodyParser.json({ limit: "50mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://admin:RDGb9kDzZJPsZthC@roy.gqjnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`It's alive on port: ${PORT}`))
   )
