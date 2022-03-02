11bae3245e069a14306a760d449b145b55be52a7
Elliot Chung <ec2018@gmail.com>
2022-03-01 20:19:57 -0800

    Added form validation

--- frontend/src/components/SignUpForm.tsx
+++ frontend/src/components/SignUpForm.tsx
@@ -1,39 +1,64 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from "react";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import FormValues from "../sharedtypes/FormValues";
 
 interface Props {
   onSubmit: (values: FormValues) => void;
 }
 
+const schema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  email: yup
+    .string()
+    .email("Must be a valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+    )
+    .required("Password is required"),
+  passwordConfirmation: yup
+    .string()
+    .oneOf([yup.ref("password")], "Passwords must match")
+    .required(),
+});
+
 function SignUpForm({ onSubmit }: Props): JSX.Element {
-  const { register, handleSubmit } = useForm<FormValues>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: yupResolver(schema),
+  });
 
   return (
     <>
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("name")} type="text" placeholder="Name" />
+        {errors.name && <p> {errors.name.message} </p>}
+        <input {...register("email")} type="email" placeholder="Email" />
+        {errors.email && <p> {errors.email.message} </p>}
         <input
-          {...register("name", { required: true })}
-          type="text"
-          placeholder="Name"
-        />
-        <input
-          {...register("email", { required: true })}
-          type="email"
-          placeholder="Email"
-        />
-        <input
-          {...register("password", { required: true })}
+          {...register("password")}
           type="password"
           placeholder="Password"
         />
+        {errors.password && <p> {errors.password.message} </p>}
         <input
-          {...register("passwordConfirmation", { required: true })}
+          {...register("passwordConfirmation")}
           type="password"
           placeholder="Password Confirmation"
         />
+        {errors.passwordConfirmation && (
+          <p> {errors.passwordConfirmation.message} </p>
+        )}
         <input type="submit" value="Submit" />
       </form>
     </>
