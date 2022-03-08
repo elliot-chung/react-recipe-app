06c4e6eb380927b75bf609e8d0431bcddeb6d539
Elliot Chung <ec2018@gmail.com>
2022-03-08 03:01:40 -0800

    Added logout button

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import LoginContext from "../contexts/LoginContext";
 import Searchbar from "./Searchbar";
 
 function Navbar(): JSX.Element {
-  const user = null;
+  const userStates = useContext(LoginContext);
+  const { isLoggedIn, logout } = userStates;
   return (
     <nav>
       <Link to="/"> Home </Link>
       <Searchbar />
       <Link to="/favorites"> Favorites </Link>
-      {user ? (
-        <Link to="/login"> Logout </Link>
+      {isLoggedIn ? (
+        <button type="button" onClick={logout}>
+          Logout
+        </button>
       ) : (
         <Link to="/login"> Login </Link>
       )}
