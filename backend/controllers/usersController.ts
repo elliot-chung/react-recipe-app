11f3f8a903069b4075a993538a3ef8e8aed58fe5
Elliot Chung <ec2018@gmail.com>
2022-03-08 02:57:56 -0800

    hashing passwords

--- backend/controllers/usersController.ts
+++ backend/controllers/usersController.ts
@@ -1,39 +1,39 @@
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 import { Request, Response } from "express";
 import User from "../models/user.model";
 import getErrorMsg from "./getErrorMsg";
 
 export async function addUser(req: Request, res: Response) {
   try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      hashedPassword: hashedPassword,
     });
-    res.send({ error: "User added successfully" });
+    res.send({ message: "User added successfully" });
   } catch (error) {
-    const errorMsg = getErrorMsg(error);
-    res.status(400).send({ error: errorMsg });
+    res.status(400).send({ error: getErrorMsg(error) });
   }
 }
 
 export async function loginUser(req: Request, res: Response) {
   try {
     const user = await User.findOne({ email: req.body.email });
-    if (user) {
-      if (user.password === req.body.password) {
-        const token = jwt.sign(
-          { name: user.name, email: user.email },
-          `${process.env.JWT_SECRET}`,
-          { expiresIn: "1h" }
-        );
-        res.send({ token: token });
-      } else {
-        res.status(400).send({ error: "Password is incorrect" });
-      }
-    } else {
-      res.status(400).send({ error: "User not found" });
-    }
+    if (!user) res.status(400).send({ error: "User not found" });
+    const isPasswordMatch = await bcrypt.compare(
+      req.body.password,
+      user.hashedPassword
+    );
+    if (!isPasswordMatch)
+      res.status(400).send({ error: "Password is incorrect" });
+    const token = jwt.sign(
+      { name: user.name, email: user.email },
+      `${process.env.JWT_SECRET}`,
+      { expiresIn: "1h" }
+    );
+    res.send({ token: token });
   } catch (error) {
     const errorMsg = getErrorMsg(error);
     res.status(400).send({ error: errorMsg });
