747a3a8f6748e9897efe74b55bf2a32edebd5a04
Elliot Chung <ec2018@gmail.com>
2022-03-08 03:03:41 -0800

    Showing error for now

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useMutation } from "react-query";
 import SignUpForm from "../components/SignUpForm";
 import FormValues from "../sharedtypes/RegisterFormValues";
@@ -9,7 +9,7 @@ function Signup(): JSX.Element {
     axios.post("http://localhost:5000/users/addUser", values);
 
   const mutation = useMutation((values: FormValues) => postUser(values));
-  const { isLoading, isError, isSuccess } = mutation;
+  const { isLoading, isError, isSuccess, error } = mutation;
 
   const onSubmit = (values: FormValues): void => {
     mutation.mutate(values);
@@ -18,7 +18,7 @@ function Signup(): JSX.Element {
   return (
     <>
       {isLoading && <p>Loading...</p>}
-      {isError && <p>Error :(</p>}
+      {isError && <p>Error :{(error as AxiosError)?.response?.data?.error}</p>}
       {isSuccess && <p>Success!</p>}
       <SignUpForm onSubmit={onSubmit} />
     </>
