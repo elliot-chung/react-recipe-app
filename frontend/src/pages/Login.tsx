7fce7694ba976f7a4632d248f6ec0550ddf69b64
Elliot Chung <ec2018@gmail.com>
2022-03-08 03:03:24 -0800

    Refactored file

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,60 +1,21 @@
 import React, { useContext } from "react";
-import axios, { AxiosError, AxiosResponse } from "axios";
-import jwt_decode from "jwt-decode";
-import { Link, useNavigate } from "react-router-dom";
-import {
-  GoogleLogin,
-  GoogleLoginResponse,
-  GoogleLoginResponseOffline,
-} from "react-google-login";
-import { useMutation } from "react-query";
+import { AxiosError } from "axios";
+import { Link } from "react-router-dom";
+import { GoogleLogin } from "react-google-login";
+import Config from "../config.json";
 import SignInForm from "../components/SignInForm";
 import FormValues from "../sharedtypes/LoginFormValues";
-import UserObj from "../sharedtypes/UserObj";
 import LoginContext from "../contexts/LoginContext";
+import useLogin from "../controllers/LoginController";
+import {
+  googleFailure,
+  googleSuccess,
+} from "../controllers/GoogleLoginController";
 
 function Login(): JSX.Element {
   const userStates = useContext(LoginContext);
-  const { isLoggedIn, login, setUser } = userStates;
-  const navigate = useNavigate();
-
-  const googleSuccess = async (
-    response: GoogleLoginResponse | GoogleLoginResponseOffline
-  ): Promise<void> => {
-    const res = response as GoogleLoginResponse;
-    const profileObj = res?.profileObj;
-    const token = res?.accessToken;
-
-    console.log(profileObj, token);
-  };
-
-  const googleFailure = (
-    response: GoogleLoginResponse | GoogleLoginResponseOffline
-  ): void => {
-    console.log(response);
-  };
-
-  const storeUserToken = (response: AxiosResponse) => {
-    const { token } = response.data;
-    localStorage.setItem("token", token);
-    const user = jwt_decode<UserObj>(token);
-    setUser({
-      name: user.name,
-      email: user.email,
-    });
-    login();
-    navigate("/");
-  };
-
-  const loginUser = async (values: FormValues): Promise<AxiosResponse> =>
-    axios.post("http://localhost:5000/users/loginUser", values);
-
-  const { mutate, isLoading, isError, isSuccess, error } = useMutation(
-    loginUser,
-    {
-      onSuccess: storeUserToken,
-    }
-  );
+  const { isLoggedIn } = userStates;
+  const { isLoading, isError, isSuccess, error, mutate } = useLogin();
 
   const onSubmit = (values: FormValues): void => {
     mutate(values);
@@ -72,13 +33,11 @@ function Login(): JSX.Element {
   return (
     <>
       {isLoading && <p>Loading...</p>}
-      {isError && (
-        <p>Error: {(error as AxiosError)?.response?.data?.error?.message}</p>
-      )}
+      {isError && <p>Error: {(error as AxiosError)?.response?.data?.error}</p>}
       {isSuccess && <p>Success!</p>}
       <SignInForm onSubmit={onSubmit} />
       <GoogleLogin
-        clientId="853913000249-hbmvigouf2t3b0heg9noss90hk5l9g5b.apps.googleusercontent.com"
+        clientId={Config.GOOGLE_CLIENT_ID}
         buttonText="Sign in with Google"
         onSuccess={googleSuccess}
         onFailure={googleFailure}
