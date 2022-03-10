86e41b52956cf6d2921e896244bb4abd23e3a9b8
Elliot Chung <ec2018@gmail.com>
2022-03-09 22:17:11 -0800

    Removed Google login for now

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,16 +1,10 @@
 import React, { useContext } from "react";
 import { AxiosError } from "axios";
 import { Link } from "react-router-dom";
-import { GoogleLogin } from "react-google-login";
-import Config from "../config.json";
 import SignInForm from "../components/SignInForm";
 import FormValues from "../sharedtypes/LoginFormValues";
 import LoginContext from "../contexts/LoginContext";
 import useLogin from "../controllers/LoginController";
-import {
-  googleFailure,
-  googleSuccess,
-} from "../controllers/GoogleLoginController";
 
 function Login(): JSX.Element {
   const userStates = useContext(LoginContext);
@@ -31,20 +25,13 @@ function Login(): JSX.Element {
   }
 
   return (
-    <>
+    <main>
       {isLoading && <p>Loading...</p>}
       {isError && <p>Error: {(error as AxiosError)?.response?.data?.error}</p>}
       {isSuccess && <p>Success!</p>}
       <SignInForm onSubmit={onSubmit} />
-      <GoogleLogin
-        clientId={Config.GOOGLE_CLIENT_ID}
-        buttonText="Sign in with Google"
-        onSuccess={googleSuccess}
-        onFailure={googleFailure}
-        cookiePolicy="single_host_origin"
-      />
       <Link to="/signup">Sign Up</Link>
-    </>
+    </main>
   );
 }
 
