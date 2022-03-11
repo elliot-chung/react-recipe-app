89819b7042f5148dd6fd9d428755f4bdd13ebb75
Elliot Chung <ec2018@gmail.com>
2022-03-11 06:26:17 -0800

    Added gloabl reset

---
+++ frontend/src/components/styles/Global.ts
@@ -0,0 +1,183 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    html,
+    body {
+        border: 0;
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        line-height: 1.5;
+        margin: 0;
+        padding: 0;
+        width: 100vw;
+        overflow-x: hidden;
+    }
+
+    div,
+    span,
+    object,
+    iframe,
+    img,
+    table,
+    caption,
+    thead,
+    tbody,
+    tfoot,
+    tr,
+    td,
+    article,
+    aside,
+    canvas,
+    details,
+    figure,
+    hgroup,
+    menu,
+    nav,
+    footer,
+    header,
+    section,
+    summary,
+    mark,
+    audio,
+    video {
+        border: 0;
+        margin: 0;
+        padding: 0;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p,
+    blockquote,
+    pre,
+    a,
+    abbr,
+    address,
+    code,
+    del,
+    dfn,
+    em,
+    ins,
+    q,
+    samp,
+    small,
+    strong,
+    sub,
+    sup,
+    b,
+    i,
+    hr,
+    dl,
+    dt,
+    dd,
+    ol,
+    ul,
+    li,
+    fieldset,
+    legend,
+    label {
+        border: 0;
+        color: var(--color-neutrals-black);
+        font-size: 100%;
+        margin: 0;
+        padding: 0;
+        vertical-align: baseline;
+    }
+
+    article,
+    aside,
+    canvas,
+    figure,
+    figure img,
+    figcaption,
+    hgroup,
+    footer,
+    header,
+    nav,
+    section,
+    audio,
+    video {
+        display: block;
+    }
+
+    table {
+        border-collapse: separate;
+        border-spacing: 0;
+    }
+
+    caption,
+    th,
+    td {
+        text-align: left;
+        vertical-align: middle;
+    }
+
+    a img {
+        border: 0;
+    }
+
+    :focus {
+        outline: 0;
+    }
+
+    a,
+    a:link,
+    a:visited,
+    a:focus,
+    a:hover,
+    a:active {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    button {
+    cursor: pointer;
+    }
+
+    /* established typography */
+
+    h1 {
+        font-size: 36px;
+        font-weight: 600;
+    }
+
+    h2 {
+        font-size: 32px;
+        font-weight: 600;
+    }
+
+    h3 {
+        font-size: 28px;
+        font-weight: 600;
+    }
+
+    h4 {
+        font-size: 24px;
+    }
+
+    h5 {
+        font-size: 20px;
+    }
+
+    p {
+        font-size: 16px;
+    }
+
+    /* main body padding */
+
+    main {
+        padding: 25px;
+    }
+
+    @media (min-width: 768px) {
+    main {
+        padding: 50px;
+    }
+    }
+
+`;
+
+export default GlobalStyle;
