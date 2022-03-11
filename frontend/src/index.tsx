644b1714eb65378ddb257a0e7f0300211a8d1764
Elliot Chung <ec2018@gmail.com>
2022-03-11 06:25:18 -0800

    Removed web vitals

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./components/styles/Global";
+import { lightTheme } from "./components/styles/themes";
 import App from "./App";
 
 const queryClient = new QueryClient();
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
