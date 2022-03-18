c8f2608e712159a552a26326efbca67767a15c8d
Elliot Chung <ec2018@gmail.com>
2022-03-17 23:35:39 -0700

    Created recipe page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
-          <Route path="/recipe" element={<Recipe />} />
+          <Route path="/recipe/:id" element={<Recipe />} />
           <Route path="/search" element={<Search />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
--- frontend/src/pages/Recipe.tsx
+++ frontend/src/pages/Recipe.tsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+import RecipeInstructions from "../components/RecipeInstructions";
+import RecipeSideBar from "../components/RecipeSideBar";
+import useRecipe from "../controllers/RecipeController";
+import StyledRecipePage from "../styles/Recipe.style";
+
+let title = "";
+let imgUrl = "";
+let readyInMinutes = 0;
+let servings = 0;
+let instructions = "";
 
 function Recipe(): JSX.Element {
+  const { isError, isSuccess, data } = useRecipe();
+
+  if (isError) return <Navigate to="/error" />;
+
+  if (isSuccess) {
+    title = data?.data?.title;
+    imgUrl = data?.data?.image;
+    readyInMinutes = data?.data?.readyInMinutes;
+    servings = data?.data?.servings;
+    instructions = data?.data?.instructions;
+  }
+
   return (
-    <main>
-      <h1>This is the Recipe Page</h1>
-    </main>
+    <StyledRecipePage>
+      <RecipeSideBar
+        imgUrl={imgUrl}
+        title={title}
+        readyInMinutes={readyInMinutes}
+        servings={servings}
+      />
+      <div>
+        <RecipeInstructions instructions={instructions} />
+      </div>
+    </StyledRecipePage>
   );
 }
 
