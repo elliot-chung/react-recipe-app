136b551b6508cb50ccb762d0fc5dd34170e23149
Elliot Chung <ec2018@gmail.com>
2022-03-18 03:23:40 -0700

    Passed down state to favorite heart component

--- frontend/src/components/RecipeSideBar.tsx
+++ frontend/src/components/RecipeSideBar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import placeholder from "../assets/images/imageplaceholder.jpg";
+import FavoriteHeart from "./FavoriteHeart";
 
 type RecipeSidebarProps = {
   imgUrl: string;
   title: string;
   readyInMinutes: number;
   servings: number;
+  favoriteState: string;
+  setShowModal: (state: boolean) => void;
 };
 
 const loading = "Loading...";
@@ -15,14 +18,20 @@ function RecipeSideBar({
   title,
   readyInMinutes,
   servings,
+  favoriteState,
+  setShowModal,
 }: RecipeSidebarProps) {
   return (
-    <div>
+    <section>
       <img src={imgUrl || placeholder} alt={title} />
       <h1>{title || loading}</h1>
       <p>Ready in {readyInMinutes || loading} minutes</p>
       <p>Servings: {servings || loading}</p>
-    </div>
+      <FavoriteHeart
+        favoriteState={favoriteState}
+        setShowModal={setShowModal}
+      />
+    </section>
   );
 }
 
