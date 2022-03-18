e8816476165519e4a0e1f2f5eaefee12992386e6
Elliot Chung <ec2018@gmail.com>
2022-03-18 09:51:57 -0700

    Syled recipe instructions

---
+++ frontend/src/styles/RecipeInstructions.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledRecipeInstructions = styled.section`
+  padding: 20px;
+
+  ol {
+    padding-left: 1em;
+  }
+`;
+
+export default StyledRecipeInstructions;
