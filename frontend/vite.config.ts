fe67761e4b0de35a0af8d9978f37585492bdad66
Elliot Chung <ec2018@gmail.com>
2022-03-18 00:24:41 -0700

    Removed lint precompile (editor shows lint errors)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
 
 // https://viteejs.dev/config/
 export default defineConfig({
-  plugins: [eslint(), react()],
+  plugins: [react()],
   optimizeDeps: {
     include: ["react", "react-dom", "react-router-dom"],
   },
