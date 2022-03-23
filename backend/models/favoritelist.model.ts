f91743ea9bbca54472d8cbc0fa4112587df1524c
Elliot Chung <ec2018@gmail.com>
2022-03-23 12:26:04 -0700

    Defined schema for favorite list

---
+++ backend/models/favoritelist.model.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const FavoriteList = new mongoose.Schema(
+  {
+    listName: {
+      type: String,
+      maxlength: [100, "Name must be less than 100 characters"],
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    listItems: [
+      {
+        title: {
+          type: String,
+          maxlength: [100, "Title must be less than 100 characters"],
+          required: true,
+        },
+        imageUrl: {
+          type: String,
+          maxlength: [2048, "ImageUrl must be less than 2048 characters"],
+          required: true,
+        },
+        recipeId: {
+          type: Number,
+          required: true,
+        },
+        timeOfFavorite: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { collection: "favoritelists" }
+);
+
+const model = mongoose.model("FavoriteList", FavoriteList);
+
+export default model;
