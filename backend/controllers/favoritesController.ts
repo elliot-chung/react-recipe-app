2822c40381059512365419de4d1dc81f79529160
Elliot Chung <ec2018@gmail.com>
2022-03-23 15:52:32 -0700

    Wrote favorite controller and its routes

---
+++ backend/controllers/favoritesController.ts
@@ -0,0 +1,103 @@
+import { Request, Response } from "express";
+import User from "../models/user.model";
+import FavoriteList from "../models/favoritelist.model";
+
+interface Recipe {
+  title: string;
+  imageUrl: string;
+  recipeId: number;
+}
+
+function checkInvalidUser(req: Request, res: Response) {
+  if (!req.user) res.status(401).send("You are not logged in");
+  return !req.user;
+}
+
+async function addRecipetoList(userId: string, recipe: Recipe, listId: string) {
+  let list = await FavoriteList.findById(listId);
+  if (list.userId !== userId) return;
+  list.listItems.push(recipe);
+  await list.save();
+  return list;
+}
+
+async function addEmptyListDB(userId: string, listName: string) {
+  const user = await User.findById(userId);
+  const list = await FavoriteList.create({
+    listName: listName,
+    userId: userId,
+    listItems: [],
+  });
+  user.favorites.push(list._id);
+  await user.save();
+  return list;
+}
+
+export async function getFavorites(req: Request, res: Response) {
+  if (checkInvalidUser(req, res)) return;
+  const userId: string = req.user?.id || "";
+  const user = await User.findById(userId);
+  if (!user) return res.status(404).send("User not found");
+  const favorites = user.favorites;
+  const lists = await FavoriteList.find({ _id: { $in: favorites } });
+  res.status(200).send(lists);
+}
+
+// Request should contain a single recipe id and a list of favoritelist ids
+export async function addFavorite(req: Request, res: Response) {
+  if (checkInvalidUser(req, res)) return;
+  const userId: string = req.user?.id || "";
+  const recipe: Recipe = req.body.recipe;
+  const listIds: [string] = req.body.listIds;
+  const listNames: [string] = req.body.listNames;
+  if (!userId || !recipe || !(listIds || listNames))
+    return res.status(400).send("Malformed request");
+
+  const newListPromises = listNames.map(listName => {
+    try {
+      return addEmptyListDB(userId, listName);
+    } catch (error) {
+      return res.status(500).send("Error creating list");
+    }
+  });
+
+  const newLists = await Promise.all(newListPromises);
+  if (listNames.length !== newLists.length)
+    return res.status(500).send("Error creating new lists");
+
+  const newListIds = newLists.map(list => list._id);
+  const listIdsToAddTo = listIds.concat(newListIds);
+
+  const addToListPromises = listIdsToAddTo.map(listId => {
+    try {
+      return addRecipetoList(userId, recipe, listId);
+    } catch {
+      return res.status(500).send("Error adding recipe to list");
+    }
+  });
+
+  const finalLists = await Promise.all(addToListPromises);
+  if (listIdsToAddTo.length !== finalLists.length)
+    return res.status(500).send("Error adding recipe to lists");
+  return res.status(200).send("Added to favorites");
+}
+
+export function removeFavorites(req: Request, res: Response) {
+  if (checkInvalidUser(req, res)) return;
+}
+
+export function moveFavorites(req: Request, res: Response) {
+  if (checkInvalidUser(req, res)) return;
+}
+
+export function copyFavorites(req: Request, res: Response) {
+  if (checkInvalidUser(req, res)) return;
+}
+
+export function addEmptyList(req: Request, res: Response) {
+  if (checkInvalidUser(req, res)) return;
+}
+
+export function removeList(req: Request, res: Response) {
+  if (checkInvalidUser(req, res)) return;
+}
--- backend/routes/favoritesRoute.ts
+++ backend/routes/favoritesRoute.ts
@@ -1,5 +1,22 @@
 import express from "express";
+import {
+  addFavorite,
+  copyFavorites,
+  getFavorites,
+  moveFavorites,
+  removeFavorites,
+  addEmptyList,
+  removeList,
+} from "../controllers/favoritesController";
 
 const router = express.Router();
+
+router.get("/getFavorites", getFavorites);
+router.put("/addFavorite", addFavorite);
+router.delete("/removeFavorites", removeFavorites);
+router.patch("/moveFavorites", moveFavorites);
+router.patch("/copyFavorites", copyFavorites);
+router.put("/addEmptyList", addEmptyList);
+router.delete("/removeList", removeList);
 
 export default router;
