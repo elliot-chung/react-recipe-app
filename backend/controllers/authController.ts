747a17e20a050c3ea7a8909b2b74e4c44671f416
Elliot Chung <ec2018@gmail.com>
2022-03-23 13:05:00 -0700

    cleanup

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,11 +1,7 @@
 import { Request, Response } from "express";
 
 export function checkToken(req: Request, res: Response) {
-  if (req.user) {
-    res.status(200).send(req.user);
-  } else {
-    res.status(401).send({
-      message: "You are not logged in",
-    });
-  }
+  if (!req.user) return res.status(401).send("You are not logged in");
+
+  res.status(200).send(req.user);
 }
