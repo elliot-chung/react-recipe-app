868bf8598dd6350f171825bedc0b59bb4ddc8295
Elliot Chung <ec2018@gmail.com>
2022-03-25 00:31:52 -0700

    Added some defenitions

--- backend/environment.d.ts
+++ backend/environment.d.ts
@@ -4,6 +4,17 @@ declare global {
       PORT: string;
       CONNECTION_URL: string;
       JWT_SECRET: string;
+      RAPID_API_HOST: string;
+      RAPID_API_KEY: string;
+    }
+  }
+  namespace Express {
+    interface Request {
+      user?: {
+        name: string;
+        email: string;
+        id: string;
+      };
     }
   }
 }
