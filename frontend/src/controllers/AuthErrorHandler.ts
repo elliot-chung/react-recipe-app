eea9c4c6965631428c929a936b3115b65aeaa78b
Elliot Chung <ec2018@gmail.com>
2022-03-27 02:42:57 -0700

    Removed error handler

--- frontend/src/controllers/AuthErrorHandler.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosResponse } from "axios";
-
-function handleAPIResponse(response: AxiosResponse) {
-  if (response.status === 200) return response.data;
-  throw Error(response.data.message);
-}
-
-export default handleAPIResponse;
